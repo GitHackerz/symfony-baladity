640340231e9e44c1c061caee8efb778b4399a0c4
Sarra170 <sarra.abben@esprit.tn>
2024-03-26 03:26:36 +0100

    crud

---
+++ src/Controller/AssociationController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Association;
+use App\Form\AssociationType;
+use App\Repository\AssociationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/association')]
+class AssociationController extends AbstractController
+{
+    #[Route('/', name: 'app_association_index', methods: ['GET'])]
+    public function index(AssociationRepository $associationRepository): Response
+    {
+        return $this->render('association/index.html.twig', [
+            'associations' => $associationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_association_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $association = new Association();
+        $form = $this->createForm(AssociationType::class, $association);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($association);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_association_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('association/new.html.twig', [
+            'association' => $association,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_association_show', methods: ['GET'])]
+    public function show(Association $association): Response
+    {
+        return $this->render('association/show.html.twig', [
+            'association' => $association,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_association_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Association $association, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AssociationType::class, $association);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_association_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('association/edit.html.twig', [
+            'association' => $association,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_association_delete', methods: ['POST'])]
+    public function delete(Request $request, Association $association, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$association->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($association);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_association_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/AssociationType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Association;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AssociationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('adresse')
+            ->add('caisse')
+            ->add('type')
+            ->add('statut')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Association::class,
+        ]);
+    }
+}
