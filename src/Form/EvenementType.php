8eed6a5d4cd6e168e82b846f8c29bea67b809471
omarbtr <omar.bentahar@esprit.tn>
2024-03-27 13:28:18 +0100

    .

---
+++ src/Form/EvenementType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('date')
+            ->add('lieu')
+            ->add('nomContact')
+            ->add('emailContact')
+            ->add('statut')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
