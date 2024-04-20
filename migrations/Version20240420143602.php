b068331858b87387a605ce6b4b1feea8db07c5fe
GitHackerz <mohamedhabiballah.bibani@esprit.tn>
2024-04-20 15:41:45 +0100

    fix merge issues

--- migrations/Version20240420143602.php
+++ migrations/Version20240420143602.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240414184308 extends AbstractMigration
+final class Version20240420143602 extends AbstractMigration
 {
     public function getDescription(): string
     {
