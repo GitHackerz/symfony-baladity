f64808bce69361ff806ca378d6466b7cc13ef176
emiiiiiir <mohamedemir.ferchichi@esprit.tn>
2024-04-28 16:06:41 +0200

    mail

--- migrations/Version20240428130022.php
+++ migrations/Version20240428130022.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240428125254 extends AbstractMigration
+final class Version20240428130022 extends AbstractMigration
 {
     public function getDescription(): string
     {
