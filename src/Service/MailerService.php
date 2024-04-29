26b14e48f1a5b3c2db3198490570890cb90f50e9
GitHackerz <mohamedhabiballah.bibani@esprit.tn>
2024-04-29 17:49:13 +0100

    fix merge bugs

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -12,11 +12,19 @@ class MailerService
 {
     private $mailer;
 
+    public function __construct(MailerInterface $mailer)
+    {
         $this->mailer = $mailer;
     }
+
+    public function sendEmail(string $recipient, string $subject, string $body): void
     {
         $email = (new Email())
+            ->from(new Address("no-reply@baladity.com", "Baladity"))
+            ->to($recipient)
             ->subject($subject)
-             $this->mailer->send($email);
+            ->html($body);
+
+        $this->mailer->send($email);
     }
 }
